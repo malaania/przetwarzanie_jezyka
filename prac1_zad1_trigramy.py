00269a66487f1e3ae431395a5162e9d685af93b7
Ania <malaania@malaania-NC10-N110.(none)>
2015-10-19 17:37:59 +0200

    improve 3grams

--- prac1_zad1_trigramy.py
+++ prac1_zad1_trigramy.py
@@ -10,16 +10,17 @@ class WczytajTrigramy():
         return ngram_list
 
     @classmethod
-    def zbuduj_slownik(cls,wczytana_lista):
+    def zbuduj_slownik(cls,wczytana_lista,k):
         slownik = dict()
         for ln in wczytana_lista:
             line= ln.strip(" ").split(" ")
-            key = line[1].rstrip(" ") + " " + line[2].rstrip(" ")
-            value = line[3].rstrip("\n")
-            if key in slownik:
-                slownik[key].append(value)
-            else:
-                slownik[key]=[value]
+            if int(line[0])>k:
+                key = line[1].rstrip(" ") + " " + line[2].rstrip(" ")
+                value = line[3].rstrip("\n")
+                if key in slownik:
+                    slownik[key].append(value)
+                else:
+                    slownik[key]=[value]
         return slownik
 
     @classmethod
@@ -27,20 +28,18 @@ class WczytajTrigramy():
         zdanie=''
         slowo = random.choice(slownik.keys())
         zdanie+=slowo
-        print "'"+slowo+"'"
-        zdanie.capitalize()
         while 1:
             if slowo in slownik:
                 zdanie+=" "
                 slowo = slowo.split(" ")[1]+" "+random.choice(slownik[slowo])
-                print "'"+slowo+"'"
                 zdanie+=slowo.split(" ")[1]
             else:
                 break
-        #zdanie+="."
+        zdanie+="."
         return zdanie.capitalize()
 
 
-slownik =  WczytajTrigramy.zbuduj_slownik(WczytajTrigramy.wczytaj("/home/malaania/Desktop/ngramy/3grams", 20000))
+slownik =  WczytajTrigramy.zbuduj_slownik(WczytajTrigramy.wczytaj("/home/malaania/Desktop/ngramy/3grams", 20000),5000)
 zdanie = WczytajTrigramy.generuj_zdanie(slownik)
 print zdanie
+print len(slownik)
